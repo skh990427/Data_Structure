cd623e9083862f17d3aa3bd5cd7a40bfc2a1119b
skh990427 <skh990427@naver.com>
2023-01-19 17:40:06 +0900

    중간에 노드 추가

--- src/LinkedList/DoubleLinkedList/DoubleLinkedList.java
+++ src/LinkedList/DoubleLinkedList/DoubleLinkedList.java
@@ -44,7 +44,7 @@ class MyDoubleLinkedList<T> {
         }
     }
 
-    public T searchFromHead(T isData) {
+    public T searchFromHead(T isData) { //헤드부분에서 시작하여 값 서치
         if(this.head == null)
             return null;
         else {
@@ -60,7 +60,7 @@ class MyDoubleLinkedList<T> {
         }
     }
 
-    public T searchFromTail(T isData) {
+    public T searchFromTail(T isData) { //꼬리부분에서 시작하여 값 서치
         if(this.head == null)
             return null;
         else {
@@ -73,6 +73,35 @@ class MyDoubleLinkedList<T> {
                 }
             }
             return null;
+        }
+    }
+
+    public boolean PrevFrontAddNode(T isData, T data) { //isData 앞에 data를 추가하기
+        if(this.head == null)               //머리가 null이면 false리턴
+            return false;
+        else {
+            Node<T> node = this.head;
+            if(node.data == isData) {
+                Node<T> addnode = new Node<>(data);
+
+                this.head = addnode;        //현재의 헤드를 추가된 노드로 변경
+                addnode.next = node;        //추가된 노드의 다음노드는 미리 헤드를 넣어뒀던 node
+                node.prev = addnode;        //node의 이전노드를 추가된 노드로 변경
+                return true;
+            }
+
+            while (node.next != null) {
+                if (node.data == isData) {  //리스트를 순회하다가 찾고있는 데이터와 만나면
+                    Node<T> addnode = new Node<>(data);
+
+                    addnode.prev = node.prev;   //추가노드의 이전노드는 기존의 노드의 앞부분으로 지정
+                    addnode.next = node;        //추가노드의 다음노드는 기존의 노드로 지정
+                    node.prev.next = addnode;   //선택된 노드 이전의 다음노드를 추가노드로 지정
+                    return true;
+                }
+                node = node.next; //if문에 들어가지 못한다면 node를 다음노드로 순회
+            }
+            return false; //while을 빠져나올때까지 if문에서 노드선택을 못했다면, false 리턴
         }
     }
 }
@@ -91,5 +120,15 @@ public class DoubleLinkedList {
 
         System.out.println("머리부터 찾기 : " + mdll.searchFromHead(10));
         System.out.println("꼬리부터 찾기 : " + mdll.searchFromTail(5));
+
+        System.out.println("나는 8앞에 10을 추가할꺼야!");
+        mdll.PrevFrontAddNode(8, 10);
+
+        mdll.printAll();
+
+        System.out.println("나는 2앞에 11을 추가할꺼야!");
+        mdll.PrevFrontAddNode(2, 11);
+
+        mdll.printAll();
     }
 }
