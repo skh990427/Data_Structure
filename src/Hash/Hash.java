a1680f595a7ebb08d67b14a8a2865987d770ee70
skh990427 <skh990427@naver.com>
2023-01-30 17:31:49 +0900

    해쉬테이블 데이터 추가

--- src/Hash/Hash.java
+++ src/Hash/Hash.java
@@ -13,6 +13,23 @@ class MyHash { //해쉬 클래스
             this.value = value;
         }
     }
+
+    public int HashFunc(String key) {
+        return (int)(key.charAt(0)) % this.hashTable.length;
+    }
+
+    /**
+     * 해쉬 테이블에 데이터 추가
+     */
+    public boolean saveData(String key, String value) { 
+    Integer address = this.HashFunc(key);
+        if(this.hashTable[address] != null) {
+            this.hashTable[address].value = value;
+        } else {
+            this.hashTable[address] = new Slot(value);
+        }
+        return true;
+    }
 }
 
 public class Hash {
